16bb4120907f0643270b3411525aea9a7a1468e0
VarunRavi95 <varsi146@student.liu.se>
2022-09-08 21:52:10 +0200

    Testing Github actions, made modifications in documentation.

--- EuclidijPackage/R/Euclidean.R
+++ EuclidijPackage/R/Euclidean.R
@@ -1,3 +1,12 @@
+#' @title Computes The Greatest Common Divisor Of Two Numbers(Integers).
+#'
+#' @param x One of the integers of the two required to calculate the GCD
+#' @param y One of the integers of the two required to calculate the GCD
+#'
+#' @return This code returns the greatest common divisor
+#' @export
+#'
+
 euclidean  <- function(x, y){
   while (y != 0) {
     temp_divisor = y
